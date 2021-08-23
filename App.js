e50b79707891a4c816c00578b6e1d58e5e9febd3
Sophia Dew <sdew@stanford.edu>
2021-08-23 14:54:16 -0700

    connected profile to backend

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { StyleSheet, SafeAreaView} from 'react-native';
 import 'react-native-gesture-handler'
 import TabNavigation from './src/navigation/homeBottomTabNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 
+import {Auth, API, graphqlOperation} from 'aws-amplify';
 import {withAuthenticator} from 'aws-amplify-react-native';
 
+import {createUser} from './src/graphql/mutations';
+import {getUser} from './src/graphql/queries';
+
 //import backend from AWS
-import Amplify from 'aws-amplify'
-import config from './src/aws-exports'
-Amplify.configure(config)
+// import Amplify from 'aws-amplify'
+// import config from './src/aws-exports'
+// Amplify.configure(config)
 
 {/* First render of app. Calls tab navigator screens. */}
 
 const App = () => {
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      // get currently authenticated user
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+      //console.log(userInfo);
+      if (!userInfo) {
+        return;
+      }
+
+    //   // check if THE user exist in database
+      const getUserResponse = await API.graphql(
+        graphqlOperation(getUser, {id: userInfo.attributes.sub}),
+      );
+
+      if (getUserResponse.data.getUser) {
+        console.log('User already exists in database');
+        return;
+      }
+
+    //   // if it doesn't (it's newly registered user)
+    //   // then, create a new user in database
+      const newUser = {
+        id: userInfo.attributes.sub,
+        name: userInfo.username,
+        username: userInfo.username,
+        email: userInfo.attributes.email,
+        imageUri: 'https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg',
+      };
+      console.log(newUser)
+
+      await API.graphql(graphqlOperation(createUser, {input: newUser}));
+    };
+
+    fetchUser();
+  }, []);
+
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
       <StatusBar hidden='false' style="dark" /> 
--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState, useCallback} from 'react';
-import {View, FlatList, Text, Image, TouchableOpacity, StyleSheet, Alert, Modal, Button} from 'react-native';
+import {View, FlatList, Text, Image, TouchableOpacity, StyleSheet, Alert, Modal, Button, ActivityIndicator} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { useNavigation } from '@react-navigation/native'
 import headshot from '../../assets/headshot.jpeg'
@@ -10,85 +10,72 @@ import { useIsFocused } from "@react-navigation/native";
 import ProfilePost from '../../components/ProfilePost';
 import data from '../../../data/slides'
 import profileData from '../../../data/profile'
-import { color } from 'react-native-reanimated';
+//import { color } from 'react-native-reanimated';
 
-//import { Auth, API, graphqlOperation} from 'aws-amplify';
+import { Auth, API, graphqlOperation} from 'aws-amplify';
 
-//import { getUser } from '../../graphql/queries';
+import { getUser } from '../../graphql/queries';
 
-//import Amplify from 'aws-amplify'
-//import config from '../../aws-exports'
+import Amplify from 'aws-amplify'
+import config from '../../aws-exports'
 
-//Amplify.configure(config)
+Amplify.configure(config)
 
 const Profile = () => {
 
-    const [profile, setProfile] = useState(profileData);
-    const [modalVisible, setModalVisible] = useState(false);
-    const [posts, setPosts] = useState(data);
+    //const [profile, setProfile] = useState(null);
+    // const [modalVisible, setModalVisible] = useState(false);
+    const [posts, setPosts] = useState([]);
     const [username, setUsername] = useState([]);
-    const [profilePic, setProfilePic] = useState(null);
+    const [name, setName] = useState([]);
+    const [imageUri, setImageUri] = useState(null);
+    const [followers, setFollowers] = useState([]);
+    const [following, setFollowing] = useState([]);
 
     const isFocused = useIsFocused();
     const navigation = useNavigation();
 
-    // useEffect(() => {
-    //     const fetchUser = async () => {
-            
-    //         try {
-    //             const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
-    //             //check if the user exits in database
-    //             const getUserResponse = await API.graphql(
-    //                 graphqlOperation(
-    //                 getUser, {id: userInfo.attributes.sub}
-    //                 )
-    //             );
-    //             //console.log(getUserResponse.data.getUser.username)
-    //             setUsername(getUserResponse.data.getUser.username)
-    //             // console.log("POSTS:")
-    //             // console.log(getUserResponse.data.getUser.posts)
-    //             setPosts(getUserResponse.data.getUser.posts.items)
-    //             setProfilePic(getUserResponse.data.getUser.imageUri)
-    //             //console.log("ACTIVE USER: ")
-    //             console.log(profilePic);
-
-    //         } catch (e) {
-    //             console.error(e);
-    //         }
-    //     };
-    //     fetchUser();
-    // }, [isFocused])
-
     useEffect(() => {
-        console.log(profile);
-    }, [])
+        const fetchUser = async () => {
+            
+            try {
+                const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true});
+                //check if the user exits in database
+                const getUserResponse = await API.graphql(
+                    graphqlOperation(
+                    getUser, {id: userInfo.attributes.sub}
+                    )
+                );
+                console.log(getUserResponse.data.getUser)
+                //setProfile(getUserResponse.data.getUser)
+                //console.log(getUserResponse.data.getUser.username)
+                setUsername(getUserResponse.data.getUser.username)
+                setName(getUserResponse.data.getUser.name)
+                //setUsernam
+                // console.log("POSTS:")
+                //console.log(getUserResponse.data.getUser.posts)
+                setPosts(getUserResponse.data.getUser.posts.items)
+                setImageUri(getUserResponse.data.getUser.imageUri)
+                //console.log("ACTIVE USER: ")
+                //console.log(getUserResponse);
+
+            } catch (e) {
+                console.error(e);
+            }
+        };
+        fetchUser();
+    }, [isFocused])
+
+    // useEffect(() => {
+    //   console.log('PROFILE')
+    //     console.log(profile);
+    //     console.log('done')
+    // }, [])
 
     //optimized renderItem
     const renderItem = useCallback(
         ({item, index}) =>
         <ProfilePost post={item} mode='small' index={index} currentIndex={index}/> 
-        // <TouchableOpacity onLongPress={() => Alert.alert('test')}>
-        //     {/* <View style={modalStyles.centeredView}>
-        //         <Modal 
-        //             animationType="slide"
-        //             transparent={true}
-        //             visible={modalVisible}
-        //             onRequestClose={() => {
-        //                 Alert.alert("modal has been closed.")
-        //                 setModalVisible(!modalVisible);
-        //             }}
-        //         >
-        //              <View style={modalStyles.centeredView}>
-        //                 <View style={modalStyles.modalView}>
-        //                     <Text>test</Text>
-        //                     <Button onPress={() => setModalVisible(!modalVisible)} title="close"/>
-        //                 </View>
-        //             </View>
-        //         </Modal>
-        //     </View>
-        //      */}
-        //     <ProfilePost post={item} mode='small' index={index} currentIndex={index}/>
-        // </TouchableOpacity>
         , []
     );
 
@@ -97,15 +84,30 @@ const Profile = () => {
         (item) => item.id.toString(),[]
     );
 
+    // if(!profile){
+    //   <ActivityIndicator size="large" color="black"/>
+    //   return;
+    // }
+
     return(
+      
+      // <View>
+      //   {profile ? 
+      //   <Text>{profile.imageUri}</Text> :
+      //   <Text>not loaded</Text>
+      // }
+      // </View>
+
         <View style={styles.container}>
+          {/* {profile ? } */}
 
             {/* Top half UI */}
             <View style={styles.topContainer}>
-                <Image style={styles.image} source={{uri: profile.imageUri}}/>
+                {/* <Image style={styles.image} source={{uri: profile.imageUri}}/> */}
+                <Image style={styles.image} source={{uri: imageUri}}/>
                 <View style={styles.topRightContainer}>
-                    <Text style={styles.numberText}>{profile.name}</Text>
-                    <Text style={styles.usernameText}>@{profile.username}</Text>
+                    <Text style={styles.numberText}>{name}</Text>
+                    <Text style={styles.usernameText}>@{username}</Text>
                     <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("EditProfile")}>
                         {/* TODO: if getUser = current authenticated user ? edit : subscribe */}
                         <Text style={{color: 'white', fontWeight: '600', fontSize: 14}}>Edit</Text>
@@ -116,15 +118,15 @@ const Profile = () => {
             {/* Middle half UI */}
             <View style={styles.middleContainer}>
                 <View style={styles.middleTextContainer}>
-                    <Text style={styles.numberText}>{profile.posts.length}</Text>
+                    <Text style={styles.numberText}>{posts.length}</Text>
                     <Text style={styles.middleText}>   Slides   </Text>
                 </View>
                 <View style={styles.middleTextContainer}>
-                    <Text style={styles.numberText}>{profile.followers}</Text>
+                    <Text style={styles.numberText}>{followers.length}</Text>
                     <Text style={styles.middleText}>Subscribers</Text>
                 </View>
                 <View style={styles.middleTextContainer}>
-                    <Text style={styles.numberText}>{profile.following}</Text> 
+                    <Text style={styles.numberText}>{following.length}</Text> 
                     <Text style={styles.middleText}>Subscribed</Text> 
                 </View>
             </View>
@@ -133,7 +135,7 @@ const Profile = () => {
             {/* TODO: add modal on long press*/}
             <FlatList
                 contentContainerStyle={{margin:20}}  
-                data={profile.posts}
+                data={posts}
                 renderItem={renderItem}
                 keyExtractor={keyExtractor}
                 horizontal={false}
